4736a9001055ba6e505aaa07829cd4e70f524e7a
Magdalena Zydorczak <magda.zydorczak@gmail.com>
2023-12-05 16:19:42 +0100

    Working script with results

--- example.py
+++ example.py
@@ -6,10 +6,10 @@ env = Environ()
 # a = AutoModel(env, alnfile='data/examp2.ali',
 #               knowns=('5fd1', '1fdn', '1fxd'), sequence='1fdx')
 
-a = AutoModel(env, alnfile='Modeling-3D-Structure\\alignment.fasta',
-              knowns=('1CTP','3J4Q','6F14','6Y05'), sequence='my_sequence')
+a = AutoModel(env, alnfile='alignment.fasta',
+              knowns=('1CTP','3J4Q','6F14','6Y05'), sequence='my_sequence', assess_methods=(assess.DOPE,assess.GA341))
 a.starting_model = 1
-a.ending_model = 5
+a.ending_model = 1
 a.make()
 a.write("model.pdb")
 
